40aafdd768201a12c0e1dbfd1a4fa11eaf131469
josebernal321 <61957579+josebernal321@users.noreply.github.com>
2020-04-17 21:44:08 -0400

    menu principal + agregar carpeta Cestock en templates

--- Cestock/urls.py
+++ Cestock/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('PaginaPrincipal/', views.PaginaPrincipal, name='PaginaPrincipal'), 
+    path('AtencionMedica/', views.AtencionMedica, name='AtencionMedica'), 
 ]
